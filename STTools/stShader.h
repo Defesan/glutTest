5603e0b3adbc62a5e039781909bb046eadcafa73
Jake Hopkins <defesan@gmail.com>
2017-05-03 21:10:00 -0600

    Filled out the Shader header. Not as afraid of varargs as I was, and it looks like converting to STL strings is almost painless. Is there a little overhead penalty? Probably. Is it enough to make it a problem? Almost certainly not. Especially not with most of the shaders only being loaded once.

--- STTools/stShader.h
+++ STTools/stShader.h
@@ -1,8 +1,18 @@
 #ifndef __ST_SHADER_H__
 #define __ST_SHADER_H__
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <vector>
+#include <string>
+
 //I still haven't figured out why I'm using the 'st' prefix. Reference to my middle name, Stephan? And yes, BTW, that's how the nurse who wrote it down spelled it.
 //(Still haven't gotten over that)
+
+enum ST_SHADER_ATTRIBUTE {ST_ATTRIBUTE_VERTEX = 0, ST_ATTRIBUTE_COLOR, ST_ATTRIBUTE_NORMAL, ST_ATTRIBUTE_TEXTURE0, 
+                          ST_ATTRIBUTE_TEXTURE1, ST_ATTRIBUTE_TEXTURE2, ST_ATTRIBUTE_TEXTURE3, ST_ATTRIBUTE_LAST};
+
+enum ST_STOCK_SHADER {ST_IDENTITY = 0};	//Yeah, just this one for now.
 
 class STShaderManager
 {
@@ -16,7 +26,7 @@ class STShaderManager
 	  
 	  So the GLTools version has a basic stock shader pipeline that goes something like this:
 	  1)Initialize the stock shaders. This uses the included shader loading function to load 'pointers' (read GLuints) into an array. I suppose it's actually not that much data.
-	  2)Get a stock shader. Pass in the enum value for the stock shader tou want, and it'll give you the pointer for the fragment-vertex shader pair.
+	  2)Get a stock shader. Pass in the enum value for the stock shader you want, and it'll give you the pointer for the fragment-vertex shader pair.
 	  3)Use the stock shader. This loads all the necessary data in, and runs the shader.
 	  
 	  There are also some support methods(some already mentioned).
@@ -32,6 +42,25 @@ class STShaderManager
 	  So there's that. Let's put the working functions IN HERE, maybe.
 	  ...I wonder if these issues were solved in future releases? Probably, I'd imagine, but they use versions of OpenGL I can't run atm.
 	 */
+private:
+	//What do I need in here? a vector of the 'names' of the shaders...
+	std::vector<GLuint> activeShaderPointers;
+
+public:
+	STShaderManager();		//I think we can safely initialize the full shader library(which will be fairly small to start, but I'll keep adding to it) in the constructor.
+	~STShaderManager();
+	
+	GLuint getShader(GLuint index);	//Instead of just using the stock shaders, we'll be adding all of the added and compiled shaders to the array, so we need a more general function.
+	GLuint getStockShader(ST_STOCK_SHADER shader) : getShader((GLuint)shader) {};	//All of the custom shaders will be added AFTER the stock shaders, so the stock shader indices are valid.
+	
+	void runShader(GLuint shaderPointer);
+	
+	//Continuing my STL translation of the old code, we'll be using strings, not c-strings. Pretty much standard(and often best) practice in C++ now.
+	GLuint loadShaderPair(std::string vertexShaderFile, std::string fragShaderFile);
+	GLuint loadShaderPairSrc(std::string vertexShaderSrc, std::string fragShaderSrc);
+	
+	GLuint loadShaderPairWithAttributes(std::string vertexShaderFile, std::string fragShaderFile, ...);
+	GLuint loadShaderPairSrcWithAttributes(std::string vertexShaderSrc, std::string fragShaderSrc, ...);
 };
 
 
