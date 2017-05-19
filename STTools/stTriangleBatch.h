04587d9676628bc79d0641be4856a6277f3a7180
Jake Hopkins <defesan@gmail.com>
2017-05-18 21:47:57 -0600

    TrangleBatch is going to be another challenge, even though it's basically simpler, in many ways, than PrimitiveBatch. I'm going to need to plan it out.

--- STTools/stTriangleBatch.h
+++ STTools/stTriangleBatch.h
@@ -5,8 +5,20 @@
 
 class STTriangleBatch : public STBatch
 {
-	
-	
+	//So... What does GLTriangleBatch do?
+	//According to the description, it's basically a container form of the batch principle,
+	//based entirely on triangles.
+	//Adding a triangle, it checks to see if the vertices are 'close enough' to anything already in the vertex list.
+	//How? I mean, that sounds like it would be expensive.
+	//...and it is. I mean, damn. *Damn*! For each of the three vertices in the triangle, *for each of the vertices in the vertex list(!)*, it checks:
+	//each of the three elements of the vertex being tested against the vertex being tested against,
+	//each of the three elements of the normal,
+	//and both elements of the texture coordinate. That's eight tests,
+	//done three times per triangle
+	//done n times for each of those three times. To be sure, it's still O(n), which isn't *terrible*, but this is an insanely common task,
+	//done many times(once for each triangle added),
+	//and the number of vertices goes up by, presumably, at least one with every triangle! 
+	//This is going to take planning.
 };
 
 
